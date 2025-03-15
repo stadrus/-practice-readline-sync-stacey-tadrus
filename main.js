e462e9bd9929723361ff3843037ff7e1c9e0cb67
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-14 21:03:57 -0500

    Practice readline-sync

---
+++ main.js
@@ -0,0 +1,19 @@
+const readline = require('readline-sync');
+
+let name = readline.question("What is your name?");
+console.log("Hello," +name+ "!");
+
+   
+let questions = readline.question[
+       "1. How can you check the data type of a value?",
+       "2. What are the main data types?",
+       "3. What is the difference between null and undefined?",
+       "4. What will be the output of console.log(5+'5')?",
+       "5. What is === used for?"
+];
+
+for (let i = 0; i < questions.lenght; i++) {
+       let response = readline.question(questions[i] + " ");
+       console.log( ${i+1}: ${questions[i]});
+       console.log(${respose}\n);
+}
